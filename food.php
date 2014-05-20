0e7bde83e6d28aacaa2211d2cb577bbd08d221b3
Manefredo <fredalad@yahoo.com>
2014-05-20 15:56:00 -0500

    Update food.php

--- food.php
+++ food.php
@@ -10,9 +10,11 @@ $password = "";
 $database = "fooda";
 $ID = $_SESSION['user'];
 
+$database = "a8663810_users";
 
-$connect = mysql_connect("localhost", $user, $password);
+$connect = mysql_connect("mysql4.000webhost.com", "a8663810_nick", "nick12");
 @mysql_select_db($database) or ("Database not found");
+
 $query = "SELECT * FROM users WHERE username = '$ID'";
 
 $result = mysql_query($query);
@@ -24,3 +26,13 @@ while($data = mysql_fetch_array($result))
 	echo $data['food'];
 }
 ?>
+<html>
+<title>fooda</title>
+<br><br>
+<form action="home.php">
+<input type="submit" value="back"></input>
+</form>
+<form action="default.php">
+<input type="submit" value="log out"></input>
+</form>
+</html>
