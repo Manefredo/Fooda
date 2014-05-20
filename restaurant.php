f00713737c1b516c61f842abe8458deed3c8f7e3
Manefredo <fredalad@yahoo.com>
2014-05-20 15:59:00 -0500

    Update and rename restaurants.php to restaurant.php

--- restaurant.php
+++ restaurant.php
@@ -12,9 +12,11 @@ $password = "";
 $database = "fooda";
 $ID = $_SESSION['user'];
 
+$database = "a8663810_users";
 
-$connect = mysql_connect("localhost", $user, $password);
+$connect = mysql_connect("mysql4.000webhost.com", "a8663810_nick", "nick12");
 @mysql_select_db($database) or ("Database not found");
+
 $query = "SELECT * FROM users WHERE username = '$ID'";
 
 $result = mysql_query($query);
@@ -32,7 +34,7 @@ while($data = mysql_fetch_array($result))
 <form action="home.php">
 <input type="submit" value="back"></input>
 </form>
-<form action="index.html">
+<form action="default.php">
 <input type="submit" value="log out"></input>
 </form>
 </html>
