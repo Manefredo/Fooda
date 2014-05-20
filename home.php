93855d279a13d9a9066907726da56c6a94829c77
Manefredo <fredalad@yahoo.com>
2014-05-20 12:48:19 -0500

    Create home.php

---
+++ home.php
@@ -0,0 +1,19 @@
+<html>
+<title>fooda</title>
+
+<h3>Welcome</h3>
+<body>Select your restaurants or food to display where you have eaten</body>
+<br><br>
+<form action="food.php" method="post">
+<input type="submit" value ="food">
+</input>
+</form> 
+<form action="restaurant.php" method="post">
+<input type="submit" value ="restaurants">
+</input>
+</form>
+<form action="index.html">
+<input type="submit" value ="log out">
+</input>
+</form>
+</html>
